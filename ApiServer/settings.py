13a6fc2a03d2e31d90f335b86d95d7a414fdf917
Ed Scrimaglia <edgardo.scrimaglia@gmail.com>
2022-03-26 15:18:02 -0300

    Update settings.py

--- ApiServer/settings.py
+++ ApiServer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1z6pgnzs3dhiwnua&2=h2o-pdm!2_2gam#$!_e63e9_01zf&mj'
+SECRET_KEY = 'XXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
